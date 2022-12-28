14287af5868b276299f8e0866821cac1fd434210
Athirujkaewseesuk <athirujkaewseesuk>
2022-12-29 04:21:24 +0700

    Color Shader Website

---
+++ lib/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:color_shader_web/main.dart';
+
+Map<String, WidgetBuilder> routes = {
+  '/main': (BuildContext context) => const MyHomePage()
+};
+
+String initialRoute = '/main';
