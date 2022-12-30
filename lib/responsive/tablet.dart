74405e8534c629e9697089d32daff82c0bc8fe62
athirujkaewseesuk <athirujkaewseesuk@gmail.com>
2022-12-30 23:02:12 +0700

    tablet

---
+++ lib/responsive/tablet.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Tablet extends StatefulWidget {
+  const Tablet({super.key});
+
+  @override
+  State<Tablet> createState() => _TabletState();
+}
+
+class _TabletState extends State<Tablet> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.amber);
+  }
+}
