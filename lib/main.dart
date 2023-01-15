1ac5746a03e09768481234a5ef7bb56bdc22b9ed
Athirujkaewseesuk <athirujkaewseesuk>
2023-01-15 23:08:13 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:color_shader_web/routes.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
@@ -22,71 +25,159 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: ScreenTypeLayout(
+//         breakpoints:
+//             const ScreenBreakpoints(tablet: 757, desktop: 1193, watch: 0),
+//         desktop: const Desktop(),
+//         tablet: const Tablet(),
+//         mobile: const Mobile(),
+//       ),
+//     );
+//   }
+// }
+
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: ScreenTypeLayout(
-        breakpoints:
-            const ScreenBreakpoints(tablet: 757, desktop: 1193, watch: 0),
-        desktop: const Desktop(),
-        tablet: const Tablet(),
-        mobile: const Mobile(),
-      ),
+    Size screenSize = WidgetsBinding.instance.window.physicalSize;
+    double width = screenSize.width;
+    double height = screenSize.height;
+    Ratio size = Ratio(maxHeight: height, maxWidth: width);
+    return LayoutBuilder(
+      builder: ((BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > size.tablet) {
+          return const Desktop();
+        }
+        return const Mobile();
+      }),
     );
   }
 }
 
 class Desktop extends StatefulWidget {
+  // static double? maxHeight;
+  // static  double? maxWidth;
+
   const Desktop({super.key});
 
   @override
   State<Desktop> createState() => _DesktopState();
 }
 
+class Ratio {
+  double maxHeight;
+  double maxWidth;
+  static const designHeight = 840;
+  static const designWidth = 1800;
+
+  Ratio({
+    required this.maxHeight,
+    required this.maxWidth,
+  });
+
+  double get heightRatio => maxHeight / designHeight;
+  double get widthRatio => maxWidth / designWidth;
+
+  double get tablet => heightRatio * 1040;
+  double get header => heightRatio * 44;
+  double get box => heightRatio * 112.8;
+  double get footer => heightRatio * 54;
+
+  double get margin => heightRatio * 28;
+}
+
 class _DesktopState extends State<Desktop> {
   @override
   Widget build(BuildContext context) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Container(
-        width: 1080,
-        height: double.maxFinite,
-        color: Colors.red,
-        child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text(
-              'Hello',
-              style: GoogleFonts.openSans(
-                  fontSize: 36, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 38,
+    double maxHeight = MediaQuery.of(context).size.height;
+    double maxWidth = MediaQuery.of(context).size.width;
+    Ratio size = Ratio(maxHeight: maxHeight, maxWidth: maxWidth);
+    return Scaffold(
+      body: Column(
+          // crossAxisAlignment: CrossAxisAl
+          children: <Widget>[
+            Expanded(
+              child: SizedBox(
+                width: size.box * 10,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.brown,
+                        child: Row(
+                          children: [
+                            Text(
+                              maxHeight.toString(),
+                              style: const TextStyle(fontSize: 28),
+                            ),
+                            SizedBox(
+                              width: 20,
+                              height: 10,
+                              child: Container(),
+                            ),
+                            Text(
+                              maxWidth.toString(),
+                              style: const TextStyle(fontSize: 28),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: size.margin,
+                        child: Container(),
+                      ),
+                      Container(
+                        height: 28,
+                        color: Colors.amber,
+                      ),
+                      SizedBox(
+                        height: size.margin * 2,
+                        child: Container(),
+                      ),
+                      Container(
+                        height: size.box,
+                        width: size.box * 10,
+                        color: Colors.amber,
+                      ),
+                      SizedBox(
+                        height: size.margin,
+                        child: Container(),
+                      ),
+                      Container(
+                        height: size.box * 3,
+                        color: Colors.amber,
+                        child: AspectRatio(
+                          aspectRatio: 1.5/1,
+                          child: Container(
+                            color: Colors.blue,
+                          ),
+                        ),
+                      )
+                    ]),
+              ),
             ),
             Container(
-              height: 53,
-              width: 200,
-              color: Colors.blue,
-            ),
-            const SizedBox(
-              height: 56,
-            ),
-            Container(
-              height: 108,
-              width: double.maxFinite,
-              color: Colors.green,
-            ),
+              color: Colors.grey,
+              height: size.footer,
+              // width: double.infinity,
+            )
           ]),
-        ),
-      )
-    ]);
+    );
   }
 }
 
